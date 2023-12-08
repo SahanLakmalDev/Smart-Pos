60a5cb7132dcb5df56da6e6f6d19a48b3e12557c
Sahan Lakmal <sahanloser@gmail.com>
2023-12-08 23:09:04 +0530

    Finish Smart POS

---
+++ src/main/java/lk/ijse/dep11/pos/tm/Order.java
@@ -0,0 +1,18 @@
+package lk.ijse.dep11.pos.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String orderDate;
+    private String customerId;
+    private String customerName;
+    private BigDecimal orderTotal;
+}
