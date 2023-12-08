6d4ce1ccc1ccfe6943818812aea28620b8fa6e02
Sahan Lakmal <sahanloser@gmail.com>
2023-12-08 21:24:08 +0530

    Add Item TM

---
+++ src/main/java/lk/ijse/dep11/pos/db/ItemDataAccess.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep11.pos.db;
+
+public class ItemDataAccess {
+}
---
+++ src/main/java/lk/ijse/dep11/pos/tm/Item.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep11.pos.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    private String code;
+    private String description;
+    private int qty;
+    private BigDecimal unitPrice;
+}
