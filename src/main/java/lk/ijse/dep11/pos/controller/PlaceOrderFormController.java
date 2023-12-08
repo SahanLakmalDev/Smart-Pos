9309b07a99a91a6a4b1ff1fb48b0fdfd500a85fe
Sahan Lakmal <sahanloser@gmail.com>
2023-12-08 22:39:52 +0530

    Finalize the UI related logics

--- src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
@@ -132,14 +132,19 @@ public class PlaceOrderFormController {
                 .filter(item -> selectedItem.getCode().equals(item.getCode())).findFirst();
 
         if (optOrderItem.isEmpty()) {
+            JFXButton btnDelete = new JFXButton("Delete");
             OrderItem newOrderItem = new OrderItem(selectedItem.getCode(), selectedItem.getDescription(),
-                    Integer.parseInt(txtQty.getText()), selectedItem.getUnitPrice(),
-                    new JFXButton("Delete"));
+                    Integer.parseInt(txtQty.getText()), selectedItem.getUnitPrice(),btnDelete);
             tblOrderDetails.getItems().add(newOrderItem);
+            btnDelete.setOnAction(e -> {
+                tblOrderDetails.getItems().remove(newOrderItem);
+                selectedItem.setQty(selectedItem.getQty() + newOrderItem.getQty());
+                calculateOrderTotal();
+            });
             selectedItem.setQty(selectedItem.getQty() - newOrderItem.getQty());
         }else{
             OrderItem orderItem = optOrderItem.get();
-            orderItem.setQty(orderItem.getQty() +  Integer.parseInt(txtQty.getText()));
+            orderItem.setQty(orderItem.getQty() + Integer.parseInt(txtQty.getText()));
             tblOrderDetails.refresh();
             selectedItem.setQty(selectedItem.getQty() - Integer.parseInt(txtQty.getText()));
         }
@@ -149,8 +154,8 @@ public class PlaceOrderFormController {
     }
     private void calculateOrderTotal() {
         Optional<BigDecimal> orderTotal = tblOrderDetails.getItems().stream()
-                .map(OrderItem::getTotal)
-                .reduce(BigDecimal::add);
+                .map(oi -> oi.getTotal())
+                .reduce((prev, cur) -> prev.add(cur));
         lblTotal.setText("Total: Rs. " + orderTotal.get().setScale(2));
     }
 
