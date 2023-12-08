86561fa9bdd17c9ee05f6af34304603655aeb8c2
Sahan Lakmal <sahanloser@gmail.com>
2023-12-08 22:03:59 +0530

    Finalize the ManageItemFormController

--- src/test/java/lk/ijse/dep11/pos/db/OrderDataAccessTest.java
+++ src/test/java/lk/ijse/dep11/pos/db/OrderDataAccessTest.java
@@ -1,10 +1,12 @@
 package lk.ijse.dep11.pos.db;
 
 import lk.ijse.dep11.pos.tm.Customer;
+import lk.ijse.dep11.pos.tm.Item;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,10 +25,14 @@ public class OrderDataAccessTest {
 
     @Test
     void existsOrderByCustomerId() throws SQLException {
-        assertDoesNotThrow(()-> assertFalse(OrderDataAccess.existsOrderByCustomerId("ABC")));
+        assertDoesNotThrow(()->assertFalse(OrderDataAccess.existsOrderByItemCode("Crazy Item Code")));
+
+        ItemDataAccess.saveItem(new Item("II12345678", "Crazy Item", 5, new BigDecimal("1250")));
         CustomerDataAccess.saveCustomer(new Customer("ABC", "Crazy", "Panadura"));
         SingleConnectionDataSource.getInstance().getConnection().createStatement()
                 .executeUpdate("INSERT INTO \"order\" (id, customer_id) VALUES ('111111', 'ABC')");
-        assertDoesNotThrow(()-> assertTrue(OrderDataAccess.existsOrderByCustomerId("ABC")));
+        SingleConnectionDataSource.getInstance().getConnection().createStatement()
+                .executeUpdate("INSERT INTO order_item (order_id, item_code, qty, unit_price) VALUES ('111111', 'II12345678', 2, 1250.00)");
+        assertTrue(OrderDataAccess.existsOrderByItemCode("II12345678"));
     }
 }
